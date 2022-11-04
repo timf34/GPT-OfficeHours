288edf1aa94fd7cd51134650f9f74d5e4d059e73
timf34 <farrelti@tcd.ie>
2022-11-04 23:42:44 +0000

    checkpoint before I remove the bloated pieces of code to get the ideal text extracted

--- data_scripts/web_scraping.py
+++ data_scripts/web_scraping.py
@@ -30,25 +30,28 @@ class PaulGrahamScraper:
 
         for article in self.articles:
             print(article)
-            # article_html: str = request.urlopen(self.base_url + article).read()
-            article_html: str = request.urlopen("http://paulgraham.com/notnot.html").read()
+            article_html: str = request.urlopen(self.base_url + article).read()
+            # article_html: str = request.urlopen("http://paulgraham.com/notnot.html").read()
             article_soup: bs4.BeautifulSoup = BeautifulSoup(article_html, "html.parser")
 
             # Get article title from title tag
             article_title = article_soup.title.string
 
             # Get the article content, contained in the 2nd table
-            article_content = article_soup.find_all("table")[1]
+            article_content = article_soup.find_all("table")[1].text
+            # print(article_content)
 
-            # Get the text from the article content
-            article_text = article_content.get_text()
-            print(article_text)
-
-
-
+            # Remove newlines greater than 1 (i.e. \n\n\n -> \n or \n\n\n\n\n -> \n, or \n\n -> \n)
+            for i in range(2, 10):
+                article_content = article_content.replace("\n" * i, "\n")
 
 
-            break
+
+
+
+
+
+
             # TODO: note that this has probably gotten unneccesarily complicated.
             #  The recursion is to work with the case of a different file structure as we have for notnot.html
             #  However I can now see that this doesn't work for our normal essays!
