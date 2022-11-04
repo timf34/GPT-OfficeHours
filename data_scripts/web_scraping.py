408e6da3f7b7b2b48be9acae64f154b49e41f743
timf34 <farrelti@tcd.ie>
2022-11-04 20:10:46 +0000

    implemented `get_articles`

--- data_scripts/web_scraping.py
+++ data_scripts/web_scraping.py
@@ -1,21 +1,30 @@
-import requests
-from urllib import request
+import bs4
 
 from bs4 import BeautifulSoup
+from typing import List, Dict
+from urllib import request
 
 
 class PaulGrahamScraper:
     def __init__(self):
-        self.url = "http://www.paulgraham.com/articles.html"
-        self.r = requests.get(self.url)
-        self.html = request.urlopen(self.url).read()
-        self.soup = BeautifulSoup(self.html, "html.parser")
+        self.url: str = "http://www.paulgraham.com/articles.html"
+        self.html: str = request.urlopen(self.url).read()
+        self.soup: bs4.BeautifulSoup = BeautifulSoup(self.html, "html.parser")
+        self.articles: List = []
+
+    def get_articles(self) -> None:
+        # There are 4 tables; t1 -> the following 3; t2 -> the intro; t3 -> the articles; t4 -> the footer (rss link)
+        articles_table = self.soup.find_all("table")[2]
+
+        # Find all articles (they will end with '.html')
+        for link in articles_table.find_all("a"):
+            if link.get("href").endswith(".html"):
+                self.articles.append(link.get("href"))
 
 
 def main():
     scraper = PaulGrahamScraper()
-
-
+    scraper.get_articles()
 
 
 if __name__ == "__main__":
